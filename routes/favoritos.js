76437bf57fb1743e72041c93a7ffd30a1438527a
Lucas David <lucasdavid5882@gmail.com>
2021-08-20 17:05:53 -0300

    adicionada funcao de para verificar se game ja foi adicionado

--- routes/favoritos.js
+++ routes/favoritos.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const Game = require("../models/games");
+const Game = require("../models/game");
 
-router.post("/",(req,res) => {
-	const user = req.headers.user_hash;
+router.post("/",async (req,res) => {
+	const name = req.headers.user_hash;
 	const { appid,nota } = req.body;
-	res.send("test");
+	const newGame = new Game({
+		name:name,
+		appid:appid,
+		nota:nota
+	})
 	
+	try{
+		const user = await Game.find({name:name,appid:appid});		
+		if(user.length != 0){
+			return res.json({"message":"jogo já cadastrado"})
+	}catch(err){
+		res.json("err":err);
+	}
+	
+	try {
+	    await newGame.save();
+		res.json({"message":"jogo salvo com sucesso"});
+	}catch(err){
+		res.json({"err":err});
+	}	
 });
 
 
